64e4c81a1c47ed71150bc915f447f82be687099d
Harris <harris.dev.0214@gmail.com>
2026-02-25 16:57:54 +0800

    connect database and add users schema

---
+++ apps/backend/drizzle.config.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in the .env file');
+}
+
+export default defineConfig({
+  schema: './src/db/schemas',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL
+  },
+  migrations: {
+    prefix: 'timestamp'
+  }
+});
